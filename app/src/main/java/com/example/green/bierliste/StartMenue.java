261fb5cf59df12ccf0a2e29b8822f624d517a154
Joram Stiel <joram.stiel@gmx.de>
2018-02-10 13:59:50 +0100

    Bierlisten app.. gogogogo :-D

---
+++ app/src/main/java/com/example/green/bierliste/StartMenue.java
@@ -0,0 +1,22 @@
+package com.example.green.bierliste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartMenue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_menue);
+    }
+
+    public void hostButtonClicked(View view){
+        
+    }
+
+    public void joinButtonClicked(View view) {
+
+    }
+}
